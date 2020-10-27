21373f49dbdde8e29ee24a1f1588701c3424ed54
Erkam Kumru <cuberkam@gmail.com>
2020-10-27 13:55:01 +0300

    3.10.0

--- popup.js
+++ popup.js
@@ -1,84 +1,104 @@
 'use strict';
+chrome.storage.sync.get(['systemThemeMode'], function (e) {
+  if (e.systemThemeMode === false) {
+    const body = document.querySelector('#body');
+    const icon = document.querySelector('#icon');
+    const text1 = document.querySelector('#text-1');
+    const text2 = document.querySelector('#text-2');
+    const text3 = document.querySelector('#text-3');
+    const infoText = document.querySelector('#info-text');
 
-var body = document.querySelector('#body')
-var icon = document.querySelector('#icon')
-var text1 = document.querySelector('#text-1')
-var text2 = document.querySelector('#text-2')
-var text3 = document.querySelector('#text-3')
+    //#region Butona basılınca gerçekleşecek olaylar
+    document.getElementById('check').onclick = function () {
+      const theChecked = this.checked;
+      chrome.storage.sync.set({ checked: theChecked });
+      if (!theChecked) {
+        body.classList.add("bg-color-2");
+        body.classList.remove("bg-color-1");
+        icon.classList.add("icon-fill");
+        text1.classList.add("color-2");
+        text1.classList.remove("color-1");
+        text2.classList.add("color-2");
+        text2.classList.remove("color-1");
+        text3.classList.add("color-2");
+        text3.classList.remove("color-1");
+        text3.innerHTML = "ENABLE";
+        infoText.classList.add("color-2");
+        infoText.classList.remove("color-1");
+        chrome.browserAction.setIcon({
+          path: {
+            "19": "icon/lunate_light_19.png",
+            "38": "icon/lunate_light_38.png"
+          }
+        });
+      }
+      else {
+        body.classList.remove("bg-color-2");
+        body.classList.add("bg-color-1");
+        icon.classList.remove("icon-fill");
+        text1.classList.remove("color-2");
+        text1.classList.add("color-1");
+        text2.classList.remove("color-2");
+        text2.classList.add("color-1");
+        text3.classList.remove("color-2");
+        text3.classList.add("color-1");
+        text3.innerHTML = "DISABLE";
+        infoText.classList.remove("color-2");
+        infoText.classList.add("color-1");
+        chrome.browserAction.setIcon({
+          path: {
+            "19": "icon/lunate_dark_19.png",
+            "38": "icon/lunate_dark_38.png"
+          }
+        });
+      }
+    }
+    //#endregion Butona basılınca gerçekleşecek olaylar
 
-//#region Butona basılınca gerçekleşecek olaylar
-document.getElementById('check').onclick = function () {
-  var theChecked = this.checked;
-  chrome.storage.sync.set({ checked: theChecked });
-  if (!theChecked) {
-    body.classList.add("bg-color-2");
-    body.classList.remove("bg-color-1");
-    icon.classList.add("icon-fill");
-    text1.classList.add("color-2");
-    text1.classList.remove("color-1");
-    text2.classList.add("color-2");
-    text2.classList.remove("color-1");
-    text3.classList.add("color-2");
-    text3.classList.remove("color-1");
-    text3.innerHTML = "ENABLE";
-    chrome.browserAction.setIcon({path: {
-      "19": "icon/lunate_light_19.png",
-      "38": "icon/lunate_light_38.png"
-    }});
-  }
-  else {
-    body.classList.remove("bg-color-2");
-    body.classList.add("bg-color-1");
-    icon.classList.remove("icon-fill");
-    text1.classList.remove("color-2");
-    text1.classList.add("color-1");
-    text2.classList.remove("color-2");
-    text2.classList.add("color-1");
-    text3.classList.remove("color-2");
-    text3.classList.add("color-1");
-    text3.innerHTML = "DISABLE";
-    chrome.browserAction.setIcon({path: {
-      "19": "icon/lunate_dark_19.png",
-      "38": "icon/lunate_dark_38.png"
-    }});
-  }
-}
-//#endregion Butona basılınca gerçekleşecek olaylar
-
-//#region Site terk edildikten sonra hafızadaki değerin uygulanması
-chrome.storage.sync.get(['checked'], function (data) {
-  document.getElementById('check').checked = data.checked;
-  if (!data.checked) {
-    body.classList.add("bg-color-2");
-    body.classList.remove("bg-color-1");
-    icon.classList.add("icon-fill");
-    text1.classList.add("color-2");
-    text1.classList.remove("color-1");
-    text2.classList.add("color-2");
-    text2.classList.remove("color-1");
-    text3.classList.add("color-2");
-    text3.classList.remove("color-1");
-    text3.innerHTML = "ENABLE";
-    chrome.browserAction.setIcon({path: {
-      "19": "icon/lunate_light_19.png",
-      "38": "icon/lunate_light_38.png"
-    }});
-  }
-  else {
-    body.classList.remove("bg-color-2");
-    body.classList.add("bg-color-1");
-    icon.classList.remove("icon-fill");
-    text1.classList.remove("color-2");
-    text1.classList.add("color-1");
-    text2.classList.remove("color-2");
-    text2.classList.add("color-1");
-    text3.classList.remove("color-2");
-    text3.classList.add("color-1");
-    text3.innerHTML = "DISABLE";
-    chrome.browserAction.setIcon({path: {
-      "19": "icon/lunate_dark_19.png",
-      "38": "icon/lunate_dark_38.png"
-    }});
+    //#region Site terk edildikten sonra hafızadaki değerin uygulanması
+    chrome.storage.sync.get(['checked'], function (data) {
+      document.getElementById('check').checked = data.checked;
+      if (!data.checked) {
+        body.classList.add("bg-color-2");
+        body.classList.remove("bg-color-1");
+        icon.classList.add("icon-fill");
+        text1.classList.add("color-2");
+        text1.classList.remove("color-1");
+        text2.classList.add("color-2");
+        text2.classList.remove("color-1");
+        text3.classList.add("color-2");
+        text3.classList.remove("color-1");
+        text3.innerHTML = "ENABLE";
+        infoText.classList.add("color-2");
+        infoText.classList.remove("color-1");
+        chrome.browserAction.setIcon({
+          path: {
+            "19": "icon/lunate_light_19.png",
+            "38": "icon/lunate_light_38.png"
+          }
+        });
+      }
+      else {
+        body.classList.remove("bg-color-2");
+        body.classList.add("bg-color-1");
+        icon.classList.remove("icon-fill");
+        text1.classList.remove("color-2");
+        text1.classList.add("color-1");
+        text2.classList.remove("color-2");
+        text2.classList.add("color-1");
+        text3.classList.remove("color-2");
+        text3.classList.add("color-1");
+        text3.innerHTML = "DISABLE";
+        infoText.classList.remove("color-2");
+        infoText.classList.add("color-1");
+        chrome.browserAction.setIcon({
+          path: {
+            "19": "icon/lunate_dark_19.png",
+            "38": "icon/lunate_dark_38.png"
+          }
+        });
+      }
+    });
+    //#endregion Site terk edildikten sonra hafızadaki değerin uygulanması
   }
 });
-//#endregion Site terk edildikten sonra hafızadaki değerin uygulanması
--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 'use strict';
+if (localStorage.getItem("system-theme-mode") === "false") {
+    chrome.storage.sync.set({ systemThemeMode: false });
+    const introDark = "https://web.whatsapp.com/img/intro-connection-dark_0ee01153183b5ebd9b296399cc2104b0.jpg";
+    const introLight = "https://web.whatsapp.com/img/intro-connection_c98cc75f2aa905314d74375a975d2cf2.jpg";
+    const animatedDark = chrome.runtime.getURL("images/animated-doodle.png");
+    const animatedLight = "https://web.whatsapp.com/img/animated-doodle_600127bdb5f7627ede5cd4ef320f55b0.png";
+    const chatTileDark = "https://web.whatsapp.com/img/bg-chat-tile-dark_1551bf70d71a2e66cd28424270c268da.png";
+    const chatTileLight = "https://web.whatsapp.com/img/bg-chat-tile_9e8a2898faedb7db9bf5638405cf81ae.png";
+    const styleTextContent = document.querySelector("style").textContent;
 
-let introDark = "https://web.whatsapp.com/img/intro-connection-dark_0ee01153183b5ebd9b296399cc2104b0.jpg"
-let introLight = "https://web.whatsapp.com/img/intro-connection_c98cc75f2aa905314d74375a975d2cf2.jpg"
-let animatedDark = chrome.runtime.getURL("images/animated-doodle.png")
-let animatedLight = "https://web.whatsapp.com/img/animated-doodle_600127bdb5f7627ede5cd4ef320f55b0.png"
-let chatTileDark = "https://web.whatsapp.com/img/bg-chat-tile-dark_1551bf70d71a2e66cd28424270c268da.png"
-let chatTileLight = "https://web.whatsapp.com/img/bg-chat-tile_9e8a2898faedb7db9bf5638405cf81ae.png"
-let styleTextContent = document.querySelector("style").textContent
-
-const setStyle = (intro, chat, doodle) => `
+    const setStyle = (intro, chat, doodle) => `
     ${styleTextContent}
     [data-asset-intro-image], [data-asset-intro-image-dark], [data-asset-intro-image-light] {
         background-image: url("${intro}") !important
@@ -19,23 +20,30 @@ const setStyle = (intro, chat, doodle) => `
     ._3XqhJ, ._1gzBr, ._2mKzQ {
         background-image: url("${doodle}") !important;
     }
-    `
+    `;
 
-chrome.storage.sync.get(['checked'], function (data) {
-    if (data.checked) {
-        document.querySelector("body").className = "dark"
-        document.querySelector("style").textContent = setStyle(introDark, chatTileDark, animatedDark);
-    }
-});
+    chrome.storage.sync.get(['checked'], function (data) {
+        if (data.checked) {
+            document.querySelector("body").className = "dark";
+            document.querySelector("style").textContent = setStyle(introDark, chatTileDark, animatedDark);
+            localStorage.setItem("theme", "dark");
+        }
+    });
 
-chrome.storage.onChanged.addListener(function () {
-    const bodyClass = document.querySelector("body").className;
-    if (bodyClass == "dark") {
-        document.querySelector("body").className = "web"
-        document.querySelector("style").textContent = setStyle(introLight, chatTileLight, animatedLight);
-    }
-    else {
-        document.querySelector("body").className = "dark";
-        document.querySelector("style").textContent = setStyle(introDark, chatTileDark, animatedDark);
-    }
-});
+    chrome.storage.onChanged.addListener(function () {
+        const bodyClass = document.querySelector("body").className;
+        if (bodyClass == "dark") {
+            document.querySelector("body").className = "web";
+            document.querySelector("style").textContent = setStyle(introLight, chatTileLight, animatedLight);
+            localStorage.setItem("theme", "light");
+        }
+        else {
+            document.querySelector("body").className = "dark";
+            document.querySelector("style").textContent = setStyle(introDark, chatTileDark, animatedDark);
+            localStorage.setItem("theme", "dark");
+        }
+    });
+}
+else{
+    chrome.storage.sync.set({ systemThemeMode: true });
+}
